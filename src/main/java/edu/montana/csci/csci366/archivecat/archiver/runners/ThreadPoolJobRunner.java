49c1e831f18717f17a7ccb3a33cf36436d0383a9
Prad97 <pradiptadasgupta101@outlook.com>
2022-01-19 01:37:50 -0700

    commit

---
+++ src/main/java/edu/montana/csci/csci366/archivecat/archiver/runners/ThreadPoolJobRunner.java
@@ -0,0 +1,39 @@
+package edu.montana.csci.csci366.archivecat.archiver.runners;
+
+import edu.montana.csci.csci366.archivecat.archiver.jobs.AbstractDownloadJob;
+import edu.montana.csci.csci366.archivecat.archiver.jobs.DownloadJob;
+
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class ThreadPoolJobRunner implements DownloadJobRunner {
+    public void executeJobs(List<? extends DownloadJob> downloadJobs) {
+        // TODO implement - use a ThreadPoolExecutor with 10 threads to execute the jobs
+
+        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
+
+        CountDownLatch latch = new CountDownLatch(3);
+
+        for(DownloadJob downloadJob : downloadJobs)
+        {
+            var t = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    downloadJob.run();
+                    latch.countDown();
+                }
+            });
+            executor.execute(t);
+        }
+
+        try {
+            latch.await();
+        } catch (InterruptedException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+}
